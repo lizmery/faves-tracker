760fcd71d9d8cfde82a8f264abeff342d01f33a5
lizmery <l.vigil096@gmail.com>
2024-08-29 20:40:14 -0700

    update sign up page and add proxy

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import { useState } from 'react'
+import { 
+  Alert,
+  Button,
+  Label,
+  Spinner,
+  TextInput
+} from 'flowbite-react'
+import { Link, useNavigate } from 'react-router-dom'
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+export default function SignUp() {
+    const [formData, setFormData] = useState({})
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (!formData.email || !formData.password) {
+            return setErrorMessage('All fields are required')
+        }
+
+        try {
+            setLoading(true)
+            setErrorMessage(null)
+
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            })
+
+            const data = await res.json()
+            if (data.success === false) {
+                return setErrorMessage(data.message)
+            }
+
+            setLoading(false)
+
+            if (res.ok) {
+                navigate('/sign-in')
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* left column */}
+                <div className="flex-1">
+                    <Link to='/' className=''>
+                        test
+                    </Link>
+                </div>
+
+                {/* right column */}
+                <div className="flex-1">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                        <div>
+                            <Label value='Email' />
+                            <TextInput 
+                                type='email'
+                                placeholder='name@email.com'
+                                id='email'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <Label value='Password' />
+                            <TextInput 
+                                type='password'
+                                placeholder='******'
+                                id='password'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <Button type='submit' disabled={loading} className='bg-accent text-black'>
+                            {loading ? (
+                                <>
+                                    <Spinner size='sm' />
+                                    <span className='pl-3'>Loading...</span>
+                                </>
+                            ) : (
+                                'Sign Up'
+                            )}
+                        </Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Have an account?</span>
+                        <Link to='/sign-in' className='text-primary'>Sign In</Link>
+                    </div>
+
+                    {errorMessage && (
+                        <Alert className='mt-5' color='failure'>{errorMessage}</Alert>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default SignUp
