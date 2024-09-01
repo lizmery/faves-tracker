5f19603d41b9a1c399066ed930add781f1ecf137
lizmery <l.vigil096@gmail.com>
2024-09-01 09:02:21 -0700

    update user model

--- backend/api/models/user.model.js
+++ backend/api/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
+// TODO: add "NAME"
 const userSchema = new mongoose.Schema(
     {
         email: {
@@ -10,6 +11,11 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true,
+        },
+        profilePicture: {
+            type: String,
+            default: 
+                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
         },
         isAdmin: {
             type: Boolean,
