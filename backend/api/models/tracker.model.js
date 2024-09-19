09677890663e1d6f57aa86602e0124b8dd7e2ddb
lizmery <l.vigil096@gmail.com>
2024-09-19 14:38:01 -0700

    create tracker model

---
+++ backend/api/models/tracker.model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+
+const trackerSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+        },
+        by: {
+            type: String,
+            required: true,
+        },
+        rating: {
+            type: Number,
+            default: 0,
+        },
+        dateStarted: {
+            type: Date,
+        },
+        dateCompleted: {
+            type: Date,
+        },
+        tags: {
+            type: Array,
+        },
+        notes: {
+            type: String,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+    },
+)
+
+const Tracker = mongoose.model('Tracker', trackerSchema)
+
+export default Tracker
