38c55e74c909531b1ae4f9522fae7d3878b249e9
lizmery <l.vigil096@gmail.com>
2024-11-11 12:24:03 -0800

    update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      'api': {
+        target: 'https://media-tracker-delta.vercel.app',
         secure: false,
       },
     },
