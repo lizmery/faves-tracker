74c51a1340f8d29be94448c2837dd9880d98905b
lizmery <l.vigil096@gmail.com>
2024-11-12 14:16:16 -0800

    install cors pacakge

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import trackerRoutes from './routes/tracker.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -18,22 +19,30 @@ mongoose.connect(process.env.MONGO_URI)
     })
 
 const __dirname = path.resolve()
-
 const app = express()
+
+app.use(cors({
+    origin: ['https://mediatracker.vercel.app/', 'http://localhost:5173'],
+    credentials: true, 
+}))
 
 app.use(express.json())
 app.use(cookieParser())
 
+// API Routes
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/tracker', trackerRoutes)
 
 // Serve static files from Vite build
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
+app.use(express.static(path.join(__dirname, '../client/dist')))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`)
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
@@ -44,8 +53,4 @@ app.use((err, req, res, next) => {
         statusCode,
         message,
     })
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on ${process.env.PORT}`)
 })
